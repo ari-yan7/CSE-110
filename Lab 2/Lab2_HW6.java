ba435e7be7dc428fcf49441951dc3dbc2a692abd
Mubtasim Ariyan <156957137+ari-yan7@users.noreply.github.com>
2025-01-17 15:17:31 +0600

    Add files via upload

---
+++ Lab 2/Lab2_HW6.java
@@ -0,0 +1,9 @@
+public class Lab2_HW6 {
+    public static void main(String[] args) {
+        int n = 100;
+        int a = 1;
+        int L = 100;
+        int s = (n / 2) * (a + L);
+        System.out.println("The sum of the first 100 numbers is: " + s);
+    }
+}
